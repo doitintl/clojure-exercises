2e8adee81134bcc11de16edca781a89e9ab575e3
Joshua T. Fox <joshua@doit-intl.com>
2022-03-13 19:07:47 +0200

    snakecase

--- src/csv_histogram/core.clj
+++ src/csv_histogram/core.clj
@@ -15,7 +15,7 @@
   [filename]
   (slurp (str "./../../" filename)))
 
-(defn scaled_floor
+(defn scaled-floor
   [x & {p :precision}]
   (if p
     (let [scale (Math/pow 10 p)]
@@ -24,27 +24,27 @@
 
 (defn lines [file-content] (str/split-lines file-content))
 
-(defn pairs [lines_] (map #(str/split %1 #",") lines_))
-;; How do I use shortform lambda #( % )here?
-(defn pairs_with_double [pairs_] (map (fn [[k v]]
-                                        [k (Double/parseDouble v)]) pairs_))
+(defn pairs [lines] (map #(str/split %1 #",") lines))
 
-(defn by-gender [pairs_] (group-by #(first %) pairs_))
+(defn pairs-with-double [gender-and-num-as-str] (map (fn [[k v]]
+                                                       [k (Double/parseDouble v)]) gender-and-num-as-str))
+
+(defn by-gender [pairs] (group-by #(first %) pairs))
 
 (defn by-bucket [sequence & [width]]  (->> sequence
                                            (group-by (fn [x]
-                                                       (int (scaled_floor x :precision (* -1 (or width 1))))))
+                                                       (int (scaled-floor x :precision (* -1 (or width 1))))))
                                            (into {})))
 
 (defn counts-by-bucket [sequence] (let [b (by-bucket sequence) pairs (map (fn [[k, v]]
                                                                             [k, (count v)]) b)]
                                     (into {} pairs)))
 
-(defn by-gender-pairs [lines_] (by-gender (pairs_with_double (pairs lines_))))
+(defn by-gender-pairs [lines] (by-gender (pairs-with-double (pairs lines))))
 
-(defn gender-to-age-list [by-gender-pairs_] (map (fn [[gender sequence]]
+(defn gender-to-age-list [gender-and-stats] (map (fn [[gender sequence]]
                                                    [gender (map #(nth % 1) sequence)])
-                                                 by-gender-pairs_))
+                                                 gender-and-stats))
 (defn bucketed-by-gender [filename] (let [pairs (map (fn [[gender age-list]]
                                                        [gender (counts-by-bucket age-list)])
                                                      (gender-to-age-list (by-gender-pairs (lines (readfile filename)))))]
@@ -57,8 +57,8 @@
                                 (str/join "\n" (map #(str (pad pad-len (str (first %))) " " (str/join (repeat (nth % 1) ch)))
                                                     sorted)))))
 
-(defn bucketed-by-gender-to-hist [bucketed-by-gender_]
-  (let [gender_to_s (fn [gender counts] (hist-to-str counts gender))]
-    (str/join "\n\n" (map (fn [[gender  stats]] (gender_to_s gender stats)) bucketed-by-gender_))))
+(defn bucketed-by-gender-to-hist [bucketed-by-gender]
+  (let [gender-to-s (fn [gender counts] (hist-to-str counts gender))]
+    (str/join "\n\n" (map (fn [[gender  stats]] (gender-to-s gender stats)) bucketed-by-gender))))
 
 (println (bucketed-by-gender-to-hist (bucketed-by-gender "genderage.csv")))
