4bf205a15b96134684127e49d9230660898c29ae
Joshua T. Fox <joshua@doit-intl.com>
2022-04-11 12:29:32 +0300

    book exercise

--- test/book/core_test.clj
+++ test/book/core_test.clj
@@ -1,4 +1,3 @@
-
 (ns book.core-test
   (:require [clojure.test :refer :all]
             [book.core :refer :all]))
@@ -6,6 +5,6 @@
 (deftest a-test
   (testing "Search for books by substring of title."
     (is (not-any? #(= "Herman Melville" (:author %)) (search "not a book" catalog))))
-  (is (= (count (search "not a book" catalog)) 0 ))
-  (is (= (count (search "and" catalog)) 3 ))
+  (is (= (count (search "not a book" catalog)) 0))
+  (is (= (count (search "and" catalog)) 3))
   )
